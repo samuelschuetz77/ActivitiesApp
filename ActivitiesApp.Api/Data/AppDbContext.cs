8b8ff6f01214247dcf188433a407feb9df905776
samuel <samuel.schuetz6@gmail.com>
2026-03-24 07:15:28 -0600

    lets see if kubeys works

--- ActivitiesApp.Api/Data/AppDbContext.cs
+++ ActivitiesApp.Api/Data/AppDbContext.cs
@@ -13,74 +13,85 @@ namespace ActivitiesApp.Api.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Activity>()
-                .ToContainer("Activities")
-                .HasPartitionKey(a => a.City)
-                .UseETagConcurrency();
+            if (Database.ProviderName == "Npgsql.EntityFrameworkCore.PostgreSQL")
+            {
+                modelBuilder.Entity<Activity>(entity =>
+                {
+                    entity.ToTable("Activities");
+                    entity.HasKey(a => a.Id);
+                });
+            }
+            else
+            {
+                modelBuilder.Entity<Activity>()
+                    .ToContainer("Activities")
+                    .HasPartitionKey(a => a.City)
+                    .UseETagConcurrency();
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Id)
-                .ToJsonProperty("id");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Id)
+                    .ToJsonProperty("id");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.City)
-                .ToJsonProperty("City");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.City)
+                    .ToJsonProperty("City");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Name)
-                .ToJsonProperty("name");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Name)
+                    .ToJsonProperty("name");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Description)
-                .ToJsonProperty("description");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Description)
+                    .ToJsonProperty("description");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Cost)
-                .ToJsonProperty("cost");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Cost)
+                    .ToJsonProperty("cost");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Activitytime)
-                .ToJsonProperty("activitytime");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Activitytime)
+                    .ToJsonProperty("activitytime");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Latitude)
-                .ToJsonProperty("latitude");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Latitude)
+                    .ToJsonProperty("latitude");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Longitude)
-                .ToJsonProperty("longitude");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Longitude)
+                    .ToJsonProperty("longitude");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.MinAge)
-                .ToJsonProperty("minAge");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.MinAge)
+                    .ToJsonProperty("minAge");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.MaxAge)
-                .ToJsonProperty("maxAge");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.MaxAge)
+                    .ToJsonProperty("maxAge");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Category)
-                .ToJsonProperty("category");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Category)
+                    .ToJsonProperty("category");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.ImageUrl)
-                .ToJsonProperty("imageUrl");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.ImageUrl)
+                    .ToJsonProperty("imageUrl");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.PlaceId)
-                .ToJsonProperty("placeId");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.PlaceId)
+                    .ToJsonProperty("placeId");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.Rating)
-                .ToJsonProperty("rating");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.Rating)
+                    .ToJsonProperty("rating");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.UpdatedAt)
-                .ToJsonProperty("updatedAt");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.UpdatedAt)
+                    .ToJsonProperty("updatedAt");
 
-            modelBuilder.Entity<Activity>()
-                .Property(a => a.IsDeleted)
-                .ToJsonProperty("isDeleted");
+                modelBuilder.Entity<Activity>()
+                    .Property(a => a.IsDeleted)
+                    .ToJsonProperty("isDeleted");
+            }
         }
 
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
--- ActivitiesApp.Api/Program.cs
+++ ActivitiesApp.Api/Program.cs
@@ -8,20 +8,32 @@ builder.AddServiceDefaults();
 
 builder.Services.AddGrpc();
 
-// Add Cosmos DB context
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseCosmos(
-        accountEndpoint: builder.Configuration["CosmosDb:Endpoint"] ?? "https://localhost:8081/",
-        accountKey: builder.Configuration["CosmosDb:Key"] ?? "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
-        databaseName: "ActivitiesDb"
-    ));
+// Choose database provider based on environment variable
+var dbProvider = builder.Configuration["DATABASE_PROVIDER"] ?? "Cosmos";
+
+if (dbProvider == "Postgres")
+{
+    var connectionString = builder.Configuration.GetConnectionString("ActivitiesDb")
+        ?? throw new InvalidOperationException("ConnectionStrings:ActivitiesDb is required when DATABASE_PROVIDER=Postgres");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+else
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseCosmos(
+            accountEndpoint: builder.Configuration["CosmosDb:Endpoint"] ?? "https://localhost:8081/",
+            accountKey: builder.Configuration["CosmosDb:Key"] ?? "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+            databaseName: "ActivitiesDb"
+        ));
+}
 
 // Register Google Places service with HttpClient
 builder.Services.AddHttpClient<GooglePlacesService>();
 
 var app = builder.Build();
 
-// Ensure Cosmos DB is created
+// Ensure database is created
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
