6bfdcffc44c9181c86f0e4040f37f387c0cad833
Samuel <samuel.schuetz6@gmail.com>
2026-04-14 16:50:50 -0600

    Fix home tab navigation from activity detail

--- src/ActivitiesApp.Maui/Pages/HomePage.xaml.cs
+++ src/ActivitiesApp.Maui/Pages/HomePage.xaml.cs
@@ -1,12 +1,49 @@
 using ActivitiesApp.ViewModels;
+using Microsoft.AspNetCore.Components.WebView.Maui;
 
 namespace ActivitiesApp.Pages;
 
 public partial class HomePage : ContentPage
 {
+    private const string HostPage = "wwwroot/index.html";
+    private const string HomeRoute = "/";
+    private const string RootComponentSelector = "#app";
+
     public HomePage(HomeViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    public void ResetToRoot()
+    {
+        if (Dispatcher.IsDispatchRequired)
+        {
+            Dispatcher.Dispatch(ResetToRoot);
+            return;
+        }
+
+        var replacement = CreateBlazorWebView();
+
+        BlazorHost.Children.Clear();
+        BlazorHost.Children.Add(replacement);
+        HomeBlazorWebView = replacement;
+    }
+
+    private static BlazorWebView CreateBlazorWebView()
+    {
+        var view = new BlazorWebView
+        {
+            HostPage = HostPage,
+            StartPath = HomeRoute
+        };
+
+        view.RootComponents.Add(new RootComponent
+        {
+            Selector = RootComponentSelector,
+            ComponentType = typeof(Shared.Routes)
+        });
+
+        return view;
     }
 }
