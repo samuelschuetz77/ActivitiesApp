954222512d9093e3a3626ea7370fc9a91f701cac
Cael Church <caelchurch@gmail.com>
2026-04-15 22:12:40 -0600

    removed test 0 comment

--- src/ActivitiesApp.Web/Program.cs
+++ src/ActivitiesApp.Web/Program.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
-// Testing 0 down
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
